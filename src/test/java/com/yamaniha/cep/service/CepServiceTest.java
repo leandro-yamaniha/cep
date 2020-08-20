ac57dcc585a6dc99066342600b91e8aaf680ba6c
leandro-yamaniha <leandrosy@gmail.com>
2020-08-20 19:31:14 -0300

    include code coverage

--- src/test/java/com/yamaniha/cep/service/CepServiceTest.java
+++ src/test/java/com/yamaniha/cep/service/CepServiceTest.java
@@ -3,22 +3,27 @@ package com.yamaniha.cep.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.yamaniha.cep.exception.CepNotFoundException;
+import com.yamaniha.cep.service.impl.CepServiceImpl;
 
+@ExtendWith(MockitoExtension.class)
 @DisplayName("testing CepService")
 public class CepServiceTest {
 
-	
-	private CepService service;
+	@InjectMocks
+	private CepServiceImpl service;
 	
 	@Test
 	@DisplayName("identical cep found")
 	void identicalCodeFound() {
 		
-//		String id = "14403500";
-//		var result =service.findCep(id);
-//		Assertions.assertEquals(id, result.getCep());
+		String id = "14403500";
+		var result =service.findCep(id);
+		//Assertions.assertEquals(id, result.getCep());
 				
 	}
 	
